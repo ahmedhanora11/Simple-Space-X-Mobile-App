9bc7e6408956f90a11f1ab2611ded6b7c718124a
ahmedhanora11 <ahmedhanora11@gmail.com>
2023-03-04 22:25:53 +0800

    Basic Layout

--- Simple-Space-X-Mobile-App/App.tsx
+++ Simple-Space-X-Mobile-App/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { HomeScreen } from './src/screens/HomeScreen';
+import { LandingScreen } from './src/screens/LandingScreen';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Starts here!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LandingScreen />
   );
 }
 
